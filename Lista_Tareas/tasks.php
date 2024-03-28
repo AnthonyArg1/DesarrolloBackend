e70c1a76a1773d39ea40c7cc2c102793c664089e
AnthonyArg1 <parguello@unab.edu.co>
2024-03-28 16:35:24 -0500

    Ver Tareas

---
+++ Lista_Tareas/tasks.php
@@ -0,0 +1,15 @@
+<?php
+// Lee el archivo tasks.txt y muestra las tareas almacenadas
+if (file_exists("tasks.txt")) {
+    $tasks = file("tasks.txt", FILE_IGNORE_NEW_LINES);
+    //Si hay mas de una tarea muestra un boton para eliminar todas las tareas
+    if (count($tasks) > 1) {
+        echo "<form action='delete_all_tasks.php' method='post'>";
+        echo "<button type='submit' class='delete-all'>Eliminar Todas las Tareas</button>";
+        echo "</form>";
+    }
+    foreach ($tasks as $task) {
+        echo "<li>$task <button class='delete-all'><a href='delete_task.php?task=$task'>Eliminar</a></button></li>";
+    }
+}
+?>
