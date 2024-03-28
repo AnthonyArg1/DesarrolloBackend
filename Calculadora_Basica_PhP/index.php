ef801eb6a9925919c2c4271e804b60895fdae65e
AnthonyArg1 <parguello@unab.edu.co>
2024-03-28 11:58:12 -0500

    Maquetado html

---
+++ Calculadora_Basica_PhP/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="calculator">
+        <h2>Calculadora Básica en PhP</h2>
+        <form action="index.php" method="post">
+            <input type="number" name="num1" placeholder="Número 1" required>
+            <select name="operator" required>
+                <option value="">Selecciona una operación</option>
+                <option value="suma">Suma</option>
+                <option value="resta">Resta</option>
+                <option value="multiplicacion">Multiplicacion</option>
+                <option value="division">Division</option>
+            </select>
+            <input type="number" name="num2" placeholder="Número 2" required>
+            <button type="submit">Calcular</button>
+        </form>
+        <div class="result">
+            <?php include ('calculator.php') ?>
+        </div>
+    </div>
+</body>
+
+</html>
